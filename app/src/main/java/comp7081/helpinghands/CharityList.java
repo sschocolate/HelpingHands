1a9e35a39e08221f3f40494efc621f41bdeddb29
Vincent Huang <poshenhuang123@hotmail.com>
2016-10-25 16:16:07 -0700

    First Commit

---
+++ app/src/main/java/comp7081/helpinghands/CharityList.java
@@ -0,0 +1,13 @@
+package comp7081.helpinghands;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charity_list);
+    }
+}
