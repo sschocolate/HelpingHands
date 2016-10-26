8f5814698a4087635e22007c4c7c1945722336b8
Vincent Huang <poshenhuang123@hotmail.com>
2016-10-25 18:23:39 -0700

    Local database working. need to get adapter working for list view.

--- app/src/main/java/comp7081/helpinghands/CharityList.java
+++ app/src/main/java/comp7081/helpinghands/CharityList.java
@@ -1,9 +1,16 @@
 package comp7081.helpinghands;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class CharityList extends AppCompatActivity {
 
@@ -15,6 +22,91 @@ public class CharityList extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.logo);
 
+        String dbName = "charitiesDB.db";
+        SQLiteDatabase db = openOrCreateDatabase(dbName, 0, null);
 
+        if(!db.isOpen()) {
+            Toast.makeText(this, "Db Open Error", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+        //First insert the record
+        db.beginTransaction();
+        try{
+            //create a table
+            db.execSQL("CREATE TABLE IF NOT EXISTS "
+                    + "Charities"
+                    + " (id INT PRIMARY KEY, name VARCHAR , image BLOB, biography VARCHAR, donated INT);");
+
+            //Add a test entry
+            addEntry(db, 1, "Helping Hands", R.drawable.logo, "This is a test", 1000);
+
+            db.setTransactionSuccessful();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        finally
+        {
+            db.endTransaction();
+        }
+
+        Cursor dbCursor = null;
+
+        if(!db.isOpen()) {
+            Toast.makeText(this, "Db Open Error", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+        try{
+            dbCursor = db.query( "Charities", null, null, null, null, null, null); //so many nulls, I am good at databases :D
+
+            int nameCol = dbCursor.getColumnIndex("name");
+            int numberCol = dbCursor.getColumnIndex("donated");
+
+            if (dbCursor != null) {
+                dbCursor.moveToFirst();
+                if (dbCursor.getCount() != 0) {
+                    int i = 0;
+                    do {
+                        i++;
+                        String name = dbCursor.getString(nameCol);
+                        String number    = dbCursor.getString(numberCol);
+                        Toast.makeText(this, name + "  " + number, Toast.LENGTH_SHORT)
+                                .show();
+
+                    } while (dbCursor.moveToNext());
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        /*
+        dbCursor = db.query( "Charities", null, null, null, null, null, null);
+
+        ListAdapter adapter = new SimpleCursorAdapter(
+                this, // Context.
+                R.layout.custom_list_row,  // Specify the row template to use (here, two columns bound to the two retrieved cursor rows).
+                dbCursor,                                              // Pass in the cursor to bind to.
+                new String[] {"name", "image"},           // Array of cursor columns to bind to.
+                new int[] {R.id.CharityName, R.id.CharityImage},
+                0);  // Parallel array of which template objects to bind to those columns.
+
+        ListView lv = (ListView)findViewById(android.R.id.list);
+        lv.setAdapter(adapter);
+        */
+    }
+
+    public void addEntry( SQLiteDatabase db, int id, String name, int image, String bio, int donated) throws SQLiteException {
+        SQLiteDatabase database = db;
+        ContentValues cv = new ContentValues();
+        cv.put("id",    id);
+        cv.put("name",    name);
+        cv.put("image",   image);
+        cv.put("biography",   bio);
+        cv.put("donated",   donated);
+        database.insertWithOnConflict( "Charities", null, cv , SQLiteDatabase.CONFLICT_REPLACE);
     }
 }
