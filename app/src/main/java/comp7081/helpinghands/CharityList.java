3d75f6c3283411988feda125a513826a4d9c46de
Vincent Huang <poshenhuang123@hotmail.com>
2016-10-25 17:43:44 -0700

    Did some layout

--- app/src/main/java/comp7081/helpinghands/CharityList.java
+++ app/src/main/java/comp7081/helpinghands/CharityList.java
@@ -2,6 +2,8 @@ package comp7081.helpinghands;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CharityList extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CharityList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_list);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.logo);
+
+
     }
 }
